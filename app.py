af58bf025374549e1b90dbf887f8850733fd95cf
Sergio <sergiosouzaleao.pessoa@gmail.com>
2025-02-11 11:09:59 -0300

    adding information

--- app.py
+++ app.py
@@ -85,11 +85,15 @@ def load_sidebar(profile_pic):
 # Function to load the "About Me" tab
 def load_about_me():
     with tab1:
+        about_me_text = """
+        I am Sergio Pessoa, a Brazilian Computer Engineer who graduated from UFPE, currently working as a Data Scientist for the Brazilian football club Atletico Mineiro.
+        
+        In the past 5 years, I've been working as a Data Scientist on top-level projects. My main technical skills are Python, Machine Learning, Data Visualization, and Statistics, and I have experience with data problems in various areas, such as financial markets and sports.
+        
+        Studying the state-of-the-art of Data Science and Analytics is more than a job for me, it's also a hobby. I'm a big enthusiast of research and I like to develop a lot of side projects to train my skills.
+        """
         st.header("About Me")
-        st.write("""
-        This is a mock-up text about me. In the future, I will replace this with the actual information.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        """)
+        st.write(about_me_text)
 
 # Function to load the "Resume" tab
 def load_resume():
@@ -97,25 +101,38 @@ def load_resume():
         st.header("Resume")
         
         st.subheader("Experience")
-        st.write("""
-        - Mock Job Title 1 at Mock Company 1 (2020-2022)
-        - Mock Job Title 2 at Mock Company 2 (2018-2020)
-        - Mock Job Title 3 at Mock Company 3 (2016-2018)
-        - Mock Job Title 4 at Mock Company 4 (2014-2016)
-        """)
+        # Clube Atlético Mineiro
+        st.write("**Data Scientist** at **Clube Atlético Mineiro** | February 2024 - Now | Belo Horizonte - MG, Brazil")
+        st.write("- **Statistical Data Analysis**: Development of an algorithm to calculate probabilities of winning a football championship, and development of a dashboard to track the results, using Python and Statistical concepts.")
+        st.write("- **Machine Learning**: Using Python and SkLearn, I developed an algorithm to estimate the valuation of a football player, based on Machine Learning Algorithms such as random forest and linear regression.")
+        st.write("- **Monte Carlo Simulation**: Developed a Monte Carlo Simulation model to estimate how many points a team deserved to win in a past game.")
+        
+        # Insight Sport
+        st.write("**Data Scientist** at **Insight Sport** | December 2022 - February 2024 | London, UK (Remote)")
+        st.write("- **Data Engineering**: Collection and preprocessing of Skillcorner and Statsbomb data. This resulted in the creation of a unified Database stored in Amazon AWS, with Event and Tracking Data from football games ready to be used in the modeling.")
+        st.write("- **Data Analysis**: Development of a Streamlit App containing the view of football plays, created in chart format with Matplotlib.")
+        
+        # XP Inc.
+        st.write("**Data Analysis Intern** at **XP Inc.** | August 2021 - November 2022 | São Paulo - SP, Brazil (Remote)")
+        st.write("- **Natural Language Processing**: Developed an algorithm to standardize the ticker names of companies listed on the Brazilian Stock Exchange.")
+        st.write("- **Data Analysis**: Used Python to automate the generation of a pdf report, using the libraries Fpdf and Matplotlib.")
+        
+        # Pernambuco Brewery - Ambev
+        st.write("**Data Science Intern** at **Pernambuco Brewery - Ambev** | April 2021 - August 2021 | Itapissuma - PE, Brazil")
+        st.write("- **Deep Learning**: Developed a Convolutional Neural Network to count the number of beer cans lost during the line of production.")
+        
+        # Reciprev/UFPE project
+        st.write("**Data Researcher** at **Reciprev/UFPE project** | January 2020 - February 2021 | Recife - PE, Brazil")
+        st.write("- **Data Analysis**: Development of a dashboard using R, containing analysis of financial derivatives data and portfolio optimization techniques, such as Markowitz. The dashboard was bought by a Brazilian company that works with pension funds.")
         
         st.subheader("Education")
-        st.write("""
-        - Mock Degree 1 from Mock University 1 (2010-2014)
-        - Mock Degree 2 from Mock University 2 (2008-2010)
-        - Mock Degree 3 from Mock University 3 (2006-2008)
-        - Mock Degree 4 from Mock University 4 (2004-2006)
-        """)
+        st.write("Bachelor in **Computer Engineering** | 2017 - 2022 | Federal University of Pernambuco (UFPE), Recife, Brazil")
+        st.write("- Average: 8.42 of 10")
 
 # Function to load the "Portfolio" tab
 def load_portfolio():
     with tab3:
-        st.header("Portfolio")
+        st.header("Projects")
         
         # Create a grid layout for projects
         cols = st.columns(3)
@@ -158,7 +175,7 @@ def load_contact():
 # Main function
 def main():
     # Set page configuration
-    st.set_page_config(layout="wide", page_title="My Portfolio", page_icon=":briefcase:")
+    st.set_page_config(layout="wide", page_title="Sergio Pessoa", page_icon=":briefcase:")
 
     # Load profile picture
     profile_pic = Image.open("images/profile_pic.jpg")  # Replace with your profile picture path
@@ -168,7 +185,7 @@ def main():
 
     # Centered section with tabs
     global tab1, tab2, tab3, tab4
-    tab1, tab2, tab3, tab4 = st.tabs(["About Me", "Resume", "Portfolio", "Contact"])
+    tab1, tab2, tab3, tab4 = st.tabs(["About Me", "Resume", "Projects", "Contact"])
 
     # Load tabs
     load_about_me()
